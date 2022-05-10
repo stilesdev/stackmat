1ea1d91daccd0de896ba4b8304918dcbeb778e9f
Matthew Stiles <mstiles92@gmail.com>
2022-05-10 01:02:29 -0400

    docs(demo): show tooltips describing each event

--- demo/src/components/EventTile.vue
+++ demo/src/components/EventTile.vue
@@ -2,6 +2,7 @@
 import type { TimerEvent } from 'stackmat'
 import { ref, onUnmounted } from 'vue'
 import { useStackmat } from '@/composables/useStackmat'
+import { computed } from '@vue/reactivity'
 
 const props = defineProps<{
     timerEvent: TimerEvent
@@ -13,13 +14,49 @@ stackmat.on(props.timerEvent, () => {
 })
 
 const classes = ref('secondary')
+let timer: number | undefined = undefined
+
+const tooltip = computed(() => {
+    switch (props.timerEvent) {
+        case 'timerConnected':
+            return 'Fired once when the Stackmat signal is detected'
+        case 'timerDisconnected':
+            return 'Fired once when the Stackmat signal is lost'
+        case 'leftHandDown':
+            return 'Fired when the left touch sensor is activated'
+        case 'leftHandUp':
+            return 'Fired when the left touch sensor is released'
+        case 'rightHandDown':
+            return 'Fired when the right touch sensor is activated'
+        case 'rightHandUp':
+            return 'Fired when the right touch sensor is released'
+        case 'unready':
+            return 'Fired when both hands are activated but at least one is released before starting event is fired'
+        case 'ready':
+            return 'Fired when both hands are activated'
+        case 'starting':
+            return 'Fired when both hands are activated and the solve is ready to start, a short time after the ready event was fired'
+        case 'started':
+            return 'Fired when the solve is started and the timer starts counting up'
+        case 'stopped':
+            return 'Fired when the solve is stopped with a completed time'
+        case 'reset':
+            return 'Fired when the timer is reset to zero'
+        default:
+            return ''
+    }
+})
 
 function highlight() {
     classes.value = 'contrast'
 
-    setTimeout(() => {
+    if (timer) {
+        clearTimeout(timer)
+    }
+
+    timer = setTimeout(() => {
         classes.value = 'secondary'
-    }, 1000)
+    }, 200)
 }
 
 onUnmounted(() => {
@@ -28,7 +65,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <button :class="classes">{{ timerEvent }}</button>
+    <button :class="classes" :data-tooltip="tooltip">{{ timerEvent }}</button>
 </template>
 
 <style scoped>
