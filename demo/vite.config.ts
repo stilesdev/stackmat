7a89da6d25b9dcd7e484d169ea655ebf63d5ac0d
Matthew Stiles <mstiles92@gmail.com>
2022-11-10 00:20:29 -0500

    ci(demo): fix base path

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-})
+    base: mode === 'pages' ? '/stackmat/' : '/',
+}))
